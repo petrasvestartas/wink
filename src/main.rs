7adafb2c2770bccda091afad9828354ab22833f8
petrasvestartas <petrasvestartas@gmail.com>
2025-07-30 23:46:51 +0200

    ADD indexed buffer

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
 use wink::run;
-use wink::vertex::Vertex;
-
-
 
 fn main() {
 
-
-    const VERTICES: &[Vertex] = &[
-        Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-        Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-        Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
-    ];
-    
-    const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
-    
-
-    run(VERTICES, INDICES).unwrap();
+    run().unwrap();
 }
