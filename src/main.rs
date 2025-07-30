ccce67e2785f45e233c7fe1d4c1cd7dd334bb697
petrasvestartas <petrasvestartas@gmail.com>
2025-07-30 20:22:31 +0200

    ADD buffer working examples, next is a polygon example and next is earclip.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use wink::run;
+use wink::vertex::Vertex;
+
+
 
 fn main() {
-    run().unwrap();
+
+
+    const VERTICES: &[Vertex] = &[
+        Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
+        Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+        Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+    ];
+    
+    const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
+    
+
+    run(VERTICES, INDICES).unwrap();
 }
